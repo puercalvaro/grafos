f9dfff8666c004d2c4f5f72a9ce2f22f7cbaedf4
exo <enrique.lnx@gmail.com>
2020-10-10 17:10:45 -0300

    adding pop-up functionality

---
+++ js/popup.js
@@ -0,0 +1,37 @@
+const openPopupButton = document.querySelectorAll('[data-popup-target]')
+const closePopupButton = document.querySelectorAll('[data-close-button]')
+const wall = document.getElementById('wall')
+
+openPopupButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup = document.querySelector(button.dataset.popupTarget)
+        openPopup(popup)
+    })
+})
+
+closePopupButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup = button.closest('.popup')
+        closePopup(popup)
+    })
+})
+
+wall.addEventListener('click', () => {
+    const popups = document.querySelectorAll('.popup.active')
+    popups.forEach(popup => {
+        closePopup(popup)
+    });
+
+})
+
+function openPopup(popup) {
+    if (popup == null) return
+    popup.classList.add('active')
+    wall.classList.add('active')
+}
+
+function closePopup(popup) {
+    if (popup == null) return
+    popup.classList.remove('active')
+    wall.classList.remove('active')
+}
