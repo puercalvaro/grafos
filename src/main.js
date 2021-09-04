85acf0b2212cb0054be04556b4bafa6621c5c24a
puercalvaro <aaguirreu@utem.cl>
2021-09-03 22:28:45 -0400

    a

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+let port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+const customerRouter = require('../src/index')
+
+app.use('/', customerRouter)
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
